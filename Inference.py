38c20d18d869d68e34d07d0547d553cb1c2c11d2
Bae-Seong-Won <s1@khu.ac.kr>
2024-05-03 15:42:15 +0900

    수정 중

--- Inference.py
+++ Inference.py
@@ -261,7 +261,6 @@ def app():
                     paths = os.path.join(recon_data, os.path.splitext(j)[0], os.path.basename(image))
                     path = os.path.join(os.getcwd(), 'dataset', '레지스트', i)
                     print(path)
-                    print(sitk.ReadImage(path, sitk.sitkFloat32))
                     fixed, moving, _ = get_registration(paths, path)           # # fixed를 기준으로, moving 이미지를 변환
                     fixed, moving = sitk.GetArrayFromImage(fixed), sitk.GetArrayFromImage(moving)
                     mse_top, cos_top, ss_top = metrics(fixed, moving).rank()
